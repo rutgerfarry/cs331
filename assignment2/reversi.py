e3b0ba1959f38feaf6671d363fce76fadce578de
Rutger Farry <rutgerfarry@gmail.com>
2017-05-03 15:31:22 -0700

    Game works for humans. No scoring yet

--- assignment2/reversi.py
+++ assignment2/reversi.py
@@ -6,6 +6,18 @@ Usage: python3 reversi <player1 TYPE> <player2 TYPE>
 TYPE is either human or minimax
 """
 
+class IllegalMoveError(Exception):
+    def __init__(self, player, i, board):
+        super().__init__()
+        self.player = player
+        self.x = index_as_tuple(i, board)[0]
+        self.y = index_as_tuple(i, board)[1]
+
+    def __str__(self):
+        return "{} cannot move to square ({}, {})".format(self.player,
+                                                          chr(self.x + 65),
+                                                          self.y)
+
 class PlayerType:
     HUMAN = 1
     ROBOT = 2
@@ -16,9 +28,6 @@ class Player:
 
 EMPTY_SQUARE = '.'
 
-def opponent(player):
-    return Player.ONE if player is Player.TWO else Player.TWO
-
 def create_board(size):
     """ Creates a board, including correct starting locations.
     Will have a range error if size is smaller than 2. """
@@ -26,12 +35,17 @@ def create_board(size):
     board = [EMPTY_SQUARE for x in range(size**2)]
 
     mid = board_size(board) // 2
-    board = move(Player.TWO, mid - 1, mid - 1, board)
-    board = move(Player.ONE, mid, mid - 1, board)
-    board = move(Player.ONE, mid - 1, mid, board)
-    board = move(Player.TWO, mid, mid, board)
+    board = dangerously_make_move(Player.TWO, mid - 1, mid - 1, board)
+    board = dangerously_make_move(Player.ONE, mid, mid - 1, board)
+    board = dangerously_make_move(Player.ONE, mid - 1, mid, board)
+    board = dangerously_make_move(Player.TWO, mid, mid, board)
 
     return board
+
+# Helper functions
+
+def opponent(player):
+    return Player.ONE if player is Player.TWO else Player.TWO
 
 def print_board(board):
     size = board_size(board)
@@ -56,9 +70,18 @@ def index_as_tuple(i, board):
     return (i % size,
             i // size)
 
+def tuple_as_index(t, board):
+    """ Takes a board position in the form of (x,y) and returns an index """
+    size = board_size(board)
+    return t[0] + t[1] * size
+
+# Move-related functions
+
 def find_bracket(i, player, delta, board):
     """" Returns the valid move for a player from index i
     in the given direction (delta) """
+
+    # print("i: {}, player: {}, delta: {}".format(i, player, delta))
 
     start = i
     is_valid = lambda i: 0 <= i < len(board)
@@ -70,58 +93,118 @@ def find_bracket(i, player, delta, board):
         if is_valid(i + delta) and board[i + delta] == EMPTY_SQUARE:
             return i + delta
 
+def deltas(board):
+    size = board_size(board)
+
+    up, down, left, right = -size, size, -1, +1
+    up_left, up_right, down_left, down_right = -size-1, -size+1, size-1, size+1
+    return (up, down, left, right, up_right, up_left, down_right, down_left)
+
 def valid_moves(player, board):
     """ Returns the set of valid moves for the given player.
     Moves are tuples in the form of (x,y), e.g. (1,3) """
 
-    size = board_size(board)
     moves = set()
-
-    up, down, left, right = -size, size, -1, +1
-    up_left, up_right, down_left, down_right = -size-1, -size+1, size-1, size+1
-    deltas = (up, down, left, right, up_right, up_left, down_right, down_left)
-
     positions = {i for i, piece in enumerate(board) if piece == player}
 
     for p in positions:
-        moves_for_p = {find_bracket(p, player, d, board) for d in deltas}
+        moves_for_p = {find_bracket(p, player, d, board) for d in deltas(board)}
         moves = moves.union(moves_for_p)
 
     return moves
 
-def move(player, x, y, board):
-    # Allow x to be either an int or str
-    if isinstance(x, str):
-        x = ord(x.upper()) - 65
+def make_flips(i, player, delta, board):
+    positions = {i for i, piece in enumerate(board) if piece == player}
+    for p in positions:
+        if find_bracket(p, player, delta, board) == i:
+            while p != i:
+                board[p+delta] = player
+                p += delta
 
-    size = board_size(board)
-    i = x * size + y
+    return board
+
+def make_move(player, x, y, board):
+    """ User-facing move function with validation. """
+
+    i = tuple_as_index((x, y), board)
+    if i in valid_moves(player, board):
+        for d in deltas(board):
+            board = make_flips(i, player, d, board)
+    else:
+        raise IllegalMoveError(player, i, board)
+
+    return board
+
+def dangerously_make_move(player, x, y, board):
+    """ Moves without validation or flips. For internal setup use only. """
+
+    i = tuple_as_index((x, y), board)
     board[i] = player
     return board
 
-def player_type_from_argv(i):
-    """ Parses argv at the given index and returns whether user
-    has selected to play against a human or robot """
+# Strategies
+
+def human_strategy(player, board):
+    raw_move = input("Enter your move (example A1): ")
+    x = raw_move[0:1]
+    y = raw_move[1:]
+
+    if not x.isalpha():
+        print("{} is an invalid column, try again.".format(x))
+        return human_strategy(player, board)
+    if not y.isdigit():
+        print("{} is an invalid row, try again.".format(y))
+        return human_strategy(player, board)
+
+    x = ord(x.upper()) - 65
+    y = int(y)
+
+    try:
+        board = make_move(player, x, y, board)
+        return board
+
+    except IllegalMoveError:
+        print("{} is not a legal move!".format(raw_move))
+        print("Possible moves are: {}".format(valid_moves(player, board)))
+        print("Try again.")
+
+    return human_strategy(player, board)
+
+
+def player_strategy_from_argv(i):
+    """ Parses argv at the given index and returns strategy for user """
 
     # Ensure we don't look out of range
     if len(sys.argv) < i:
         sys.exit(USAGE_ERROR)
 
     if sys.argv[i].lower() == "human":
-        return PlayerType.HUMAN
-    elif sys.argv[i].lower() == "minimax":
-        return PlayerType.ROBOT
+        return human_strategy
+    # elif sys.argv[i].lower() == "minimax":
+    #     return PlayerType.ROBOT
     else:
         sys.exit(USAGE_ERROR)
 
+# Main
+
 def main():
-    player1 = player_type_from_argv(1)
-    player2 = player_type_from_argv(2)
+    player1_strat = player_strategy_from_argv(1)
+    player2_strat = player_strategy_from_argv(2)
+
+    strategy = lambda p: player1_strat if p == Player.ONE else player2_strat
+
+    current_player = Player.ONE
 
     board = create_board(4)
-    print_board(board)
 
-    print(valid_moves(Player.TWO, board))
+    while valid_moves(current_player, board) != {None}:
+        print()
+        print_board(board)
+        print("Player {}'s turn:".format(current_player))
+        board = strategy(current_player)(current_player, board)
+        current_player = opponent(current_player)
+
+    print("No more moves are possible, game over!")
 
 # Prevent running if imported as a module
 if __name__ == "__main__":
