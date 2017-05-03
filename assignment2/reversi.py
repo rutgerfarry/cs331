9cc2dbafb2a0e458844ded1c328f2c4266a0e004
Rutger Farry <rutgerfarry@gmail.com>
2017-05-03 12:04:45 -0700

    Implement `valid_moves()` func & some helper funcs

--- assignment2/reversi.py
+++ assignment2/reversi.py
@@ -16,6 +16,9 @@ class Player:
 
 EMPTY_SQUARE = '.'
 
+def opponent(player):
+    return Player.ONE if player is Player.TWO else Player.TWO
+
 def create_board(size):
     """ Creates a board, including correct starting locations.
     Will have a range error if size is smaller than 2. """
@@ -32,6 +35,7 @@ def create_board(size):
 
 def print_board(board):
     size = board_size(board)
+
     # Print letters for each column
     print('     ', end='')
     print(' '.join([chr(i + 65) for i in range(0, size)]))
@@ -43,7 +47,47 @@ def print_board(board):
         print(' '.join(board[y : y + size]))
 
 def board_size(board):
+    """ Returns size of one side of the board """
     return int(math.sqrt(len(board)))
+
+def index_as_tuple(i, board):
+    """ Takes an index and returns as a board position in form of (x,y) """
+    size = board_size(board)
+    return (i % size,
+            i // size)
+
+def find_bracket(i, player, delta, board):
+    """" Returns the valid move for a player from index i
+    in the given direction (delta) """
+
+    start = i
+    is_valid = lambda i: 0 <= i < len(board)
+
+    while is_valid(i + delta) and board[i + delta] == opponent(player):
+        i += delta
+
+    if i != start:
+        if is_valid(i + delta) and board[i + delta] == EMPTY_SQUARE:
+            return i + delta
+
+def valid_moves(player, board):
+    """ Returns the set of valid moves for the given player.
+    Moves are tuples in the form of (x,y), e.g. (1,3) """
+
+    size = board_size(board)
+    moves = set()
+
+    up, down, left, right = -size, size, -1, +1
+    up_left, up_right, down_left, down_right = -size-1, -size+1, size-1, size+1
+    deltas = (up, down, left, right, up_right, up_left, down_right, down_left)
+
+    positions = {i for i, piece in enumerate(board) if piece == player}
+
+    for p in positions:
+        moves_for_p = {find_bracket(p, player, d, board) for d in deltas}
+        moves = moves.union(moves_for_p)
+
+    return moves
 
 def move(player, x, y, board):
     # Allow x to be either an int or str
@@ -76,6 +120,8 @@ def main():
 
     board = create_board(4)
     print_board(board)
+
+    print(valid_moves(Player.TWO, board))
 
 # Prevent running if imported as a module
 if __name__ == "__main__":
