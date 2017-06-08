5c0bab7920f03b0d2ea302949fd6a10d20fe4674
Rutger Farry <rutgerfarry@gmail.com>
2017-06-08 15:09:04 -0700

    Add calculating prior for naive bayes

--- assignment3/main.py
+++ assignment3/main.py
@@ -8,12 +8,18 @@ def process_word(word):
 def process_line(line):
 # Returns a tuple. The first element is the review split into array of words.
 # The second element is an int indicating whether the review was positive.
-    return ([process_word(word) for word in line.split()][:-1], line.split()[-1])
+    return ([process_word(word) for word in line.split()][:-1], int(line.split()[-1]))
 
 def format_line(line, vocab):
     return ",".join(['1' if word in line[0] else '0' for word in vocab]) + "," + str(line[1])
 
 def preprocess(infilename, outfilename):
+# Returns a tuple in the form of (vocab, lines).
+# Vocab: a set of words, e.g. { "great", "steak", "gross", ... }
+# Lines: a tuple containing the array of words in the document followed
+# by the sentiment of the document, represented by a 1 or 0, e.g.
+# (["i", "liked", "the", "food"], 1)
+
     # Process input file
     infile = open(infilename, 'r')
     outfile = open(outfilename, 'w')
@@ -23,11 +29,24 @@ def preprocess(infilename, outfilename):
     vocab.discard('')
     infile.close()
 
-    # Process text for outfile and write
+    # Format text for outfile and write
     formatted_vocab = ",".join(sorted(list(vocab)))
     formatted_lines = "\n".join([format_line(line, vocab) for line in lines])
     outfile.write(formatted_vocab + '\n' + formatted_lines)
+    outfile.close()
 
     return vocab, lines
 
-preprocess("trainingSet.txt", "preprocessed_train.txt")
+def naive_bayes(vocab, lines):
+    def calculate_prior(lines):
+        positive_documents = len([line for line in lines if line[1] == 1])
+        total_documents = len(lines)
+        return positive_documents / total_documents
+
+    prior = calculate_prior(lines)
+    print(prior)
+
+# Prevent running if imported as a module
+if __name__ == "__main__":
+    data = preprocess("trainingSet.txt", "preprocessed_train.txt")
+    naive_bayes(*data)
